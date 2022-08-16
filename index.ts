11c2753cfc98f225c66eeb018299884139efd561
deathabel <deathabel@gmail.com>
2022-08-16 18:23:35 +0800

    2 files modified

--- index.ts
+++ index.ts
@@ -44,16 +44,42 @@ function renderGrid(data: any) {
   contentElement.innerHTML = contentTemplate;
 }
 
+/*
 request(
   'get',
   'https://raw.githubusercontent.com/deathabel/typescript-u6wttr/RxjsDemo/data.json'
 ).subscribe(renderGrid);
+*/
+
+function orderByAreaChanged(event) {
+  console.log(this.value);
+}
 
 function siteSearchInputChanged(event) {
+  console.log(this.value);
+}
+
+function filterPopulationSelectChanged(event) {
+  console.log(this.value);
+}
+
+function filterPopulationInputChanged(event) {
   console.log(this.value);
 }
 
 (document.querySelector('#searchInput') as HTMLInputElement).addEventListener(
   'keyup',
   siteSearchInputChanged
+);
+(document.querySelector('#orderBy') as HTMLInputElement).addEventListener(
+  'change',
+  orderByAreaChanged
+);
+(document.querySelector('#filter') as HTMLInputElement).addEventListener(
+  'change',
+  filterPopulationSelectChanged
+);
+(document.querySelector('#filterInput') as HTMLInputElement).addEventListener(
+  'keyup',
+  filterPopulationInputChanged
 );
